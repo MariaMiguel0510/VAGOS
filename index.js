92294dc1b312eb67746ee2e0346b194e828f4cda
MariaMiguel0510 <mariammpedro04@gmail.com>
2026-04-12 19:32:46 +0100

    landing page
    coloquei o vídeo
    acertei promenores

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 
+//ABOUT + ARCHIVE ---------------
 let about_title = document.getElementById('about_title');
 let about_container = document.querySelector('.about');
 let archive_title = document.getElementById('archive_title');
 let archive_container = document.querySelector('.archive');
-let landing_page_container = document.querySelector('.landing_page');
-let content_container = document.querySelector('.content');
 
-let visible = false;
+//CONTENT DOTS ------------------
+let dots = document.querySelectorAll('.dots a');
+let dots_container = document.querySelectorAll('.dots');
+
+window.addEventListener('scroll', () => {
+    // limpar todos
+    dots.forEach(dot => dot.style.backgroundColor = "");
+
+    if (window.scrollY < window.innerHeight - 95) {
+        dots.style.backgroundColor = "#FDF7EE";
+    } else if (window.scrollY < (2 * window.innerHeight) - 95) {
+        dots[0].style.backgroundColor = "black";
+    } else if (window.scrollY < (3 * window.innerHeight) - 95) {
+        dots[1].style.backgroundColor = "black";
+    } else {
+        dots[2].style.backgroundColor = "black";
+    }
+});
+
 
 //abrir e fechar páginas
-function toggle(page, container, openValue, closedValue) {
-    let isOpen = false; // estado inicial
-    page.addEventListener('click', function () {
-        //define o valor de 'left' baseado no estado atual
-        if (isOpen) {
-            container.style.left = closedValue;
-            page.style.zIndex = 5; //texto fica por cima
-        } else {
-            container.style.left = openValue;
-            page.style.zIndex = 0; //texto fica por baixo
-        }
+function toggle(page, container, openValue, closedValue, move_left) {
+    let isOpen = true; // estado inicial
+
+    if (move_left == true) {//se tiver escondido para a direita
         container.style.transition = 'left 0.9s ease';
+    } else {
+        container.style.transition = 'right 0.9s ease';
+    }
+
+    page.addEventListener('click', function () {
+        if (isOpen) {
+            if (move_left == true) {
+                container.style.left = closedValue;
+            } else {
+                container.style.right = closedValue;
+            }
+        } else {
+            if (move_left == true) {
+                container.style.left = openValue;
+            } else {
+                container.style.right = openValue;
+            }
+        }
 
         //alterna o estado
         isOpen = !isOpen;
     });
 }
 
-toggle(archive_title, archive_container, '100vw', '0vw');//arquivo
-toggle(about_title, about_container, '-100vw', '0vw');//sobre*/
+toggle(archive_title, archive_container, '100vw', '0vw', true);//arquivo
+toggle(about_title, about_container, '-100vw', '0vw', false);//sobre
