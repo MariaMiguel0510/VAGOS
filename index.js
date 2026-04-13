e028c9acc80f8ce3d86f024cd27be78fbd7bf2d4
MariaMiguel0510 <mariammpedro04@gmail.com>
2026-04-13 15:37:35 +0100

    logos e about

--- index.js
+++ index.js
@@ -57,3 +57,16 @@ function toggle(page, container, openValue, closedValue, move_left) {
 
 toggle(archive_title, archive_container, '100vw', '0vw', true);//arquivo
 toggle(about_title, about_container, '-100vw', '0vw', false);//sobre
+
+//bloqueia o scroll no about e no archive
+window.addEventListener('scroll', () => {
+  let rect_2 = archive_container.getBoundingClientRect();
+
+  let isVisible_2 = rect_2.left < window.innerWidth && rect_2.right > 0;
+
+  if (isVisible_2) {
+    document.body.style.overflowY = 'hidden'; //desativa scroll horizontal
+  } else {
+    document.body.style.overflowY = 'auto'; //ativa scroll horizontal 
+  }
+});
