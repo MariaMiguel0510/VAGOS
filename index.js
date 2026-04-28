dc0b9087066cc08fce786381c2cd5b533075d6ce
MariaMiguel0510 <mariammpedro04@gmail.com>
2026-04-28 12:52:27 +0100

    index.js -> landing page
    concertei o index.js para que a landing page também se mova quando abrimos/fechamos o sobre e o arquivo.
    Também eliminei a parte do ver mais+ no sobre

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ let open_about = document.getElementById('open_about');
 let close_about = document.getElementById('close_about');
 let about_container = document.querySelector('.about');
 
-let more = document.getElementById('more');
-let less = document.getElementById('less');
-let about_text = document.querySelector('.about_text');
-
 let open_archive = document.getElementById('open_archive');
 let close_archive = document.getElementById('close_archive');
 let archive_container = document.querySelector('.archive');
 
 let landing_page = document.querySelector('.landing_page')
+
 
 //CONTENT DOTS ------------------
 let dots = document.querySelectorAll('.dots a');
@@ -35,38 +32,31 @@ window.addEventListener('scroll', () => {
     }
 });
 
+
 //OPEN/CLOSE ABOUT/ARCHIVE ---------------------------------------------------
-toggle(open_archive, close_archive, archive_container, '100vw', '0vw', false);//arquivo
-toggle(open_about, close_about, about_container, '100vw', '0vw', true);//sobre
+toggleSection(open_about, close_about, about_container, 'right','100vw');//about
+toggleSection(open_archive, close_archive, archive_container, 'left','-100vw');//archive
 
-function toggle(openBtn, closeBtn, container, hiddenPosition, visiblePosition, itsabout) {
-
-    if (itsabout == false) {//se for o arqchive
-        container.style.transition = 'left 0.9s ease';
-    } else {
-        container.style.transition = 'right 0.9s ease';
-    }
+function toggleSection(openBtn, closeBtn, container, direction, landingMove) {
+    //coloca a transicao de abertura universal
+    container.style.transition = `${direction} 0.9s ease`;
+    landing_page.style.transition = `left 0.9s ease`;
 
     // ABRIR
     openBtn.addEventListener('click', function () {
-        if (itsabout == false) {//se for o archive
-            container.style.left = visiblePosition;
-        } else {
-            container.style.right = visiblePosition;
-        }
-        lockScroll(true);
+        container.style[direction] = '0vw';
+        landing_page.style.left = landingMove;
+        lockScroll(true);//bloqueia o scroll vertical
     });
 
     // FECHAR
     closeBtn.addEventListener('click', function () {
-        if (itsabout == false) {//se for o archive
-            container.style.left = hiddenPosition;
-        } else {
-            container.style.right = hiddenPosition;
-        }
-        lockScroll(false);
+        container.style[direction] = '100vw';
+        landing_page.style.left = '0vw';
+        lockScroll(false);//ativa o scroll vertical
     });
 }
+
 
 //bloqueia o scroll
 function lockScroll(lock) {
@@ -75,26 +65,4 @@ function lockScroll(lock) {
     } else {
         document.body.style.overflow = 'auto';
     }
-}
-
-
-
-//ABOUT VER MAIS -------------------------------------------------------
-toggleBtn(more, true);
-toggleBtn(less, false);
-
-function toggleBtn(botao, show) {
-    botao.addEventListener('click', function () {
-        if (show === true) { //mostra mais informação
-            more.style.display = "none";
-            less.style.display = "block";
-            about_text.style.overflow = "auto";
-            about_text.style.height = "25vh";
-        } else { //mostra menos
-            more.style.display = "block";
-            less.style.display = "none";
-            about_text.style.overflow = "hidden";
-            about_text.style.height = "16vh";
-        }
-    });
 }
