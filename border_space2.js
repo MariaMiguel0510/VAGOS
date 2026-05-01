6c236930db7ef326cb86be35ed02443f33c4982c
MariaMiguel0510 <mariammpedro04@gmail.com>
2026-05-01 19:18:39 +0100

    rects
    desenhei os retângulos por cima da borda branca (tanto nas colunas como nas linhas) e a aparecerem de forma aleatória

--- border_space2.js
+++ border_space2.js
@@ -1,24 +1,50 @@
 let container = document.querySelector('.spaces_container');
 
-space_container(container, 'div');
+// limites
+let min_c = 3;//coluna
+let max_c = 10;
+let min_r = 3;//linha
+let max_r = 6;
+
+let v = Math.floor(Math.random() * 2);//escolhe aleatoriamente 0 ou 1
+
+//desenha as retângulos
+if (v == 0) {
+    // LINHAS (topo e fundo)
+    let rowLimits = limits(min_c, max_c);
+    createRect(container, 'row', -rowLimits.start, -rowLimits.end, 1);
+    createRect(container, 'row', rowLimits.start, rowLimits.end, 8);
+} else if (v == 1) {
+    // COLUNAS (laterais)
+    let colLimits = limits(min_r, max_r);
+    createRect(container, 'col', -colLimits.start, -colLimits.end, 1);
+    createRect(container, 'col', colLimits.start, colLimits.end, 12);
+}
 
 
-function space_container(selected, name) {
-    let rect = document.createElement(name);
+function createRect(container, type, start, end, fixed) {
+    let rect = document.createElement('div');//cria o retangulo
 
-    let min = 3;
-    let max = 10;
+    rect.style.backgroundColor = '#F7F2EA';
 
+    if (type == 'row') {
+        rect.style.height = '40px';
+        rect.style.gridColumn = `${start} / ${end}`;
+        rect.style.gridRow = fixed;
+    } else if (type == 'col') {
+        rect.style.width = '40px';
+        rect.style.gridColumn = fixed;
+        rect.style.gridRow = `${start} / ${end}`;
+    }
+
+    container.appendChild(rect);
+}
+
+
+//Define os limites -> largura/altura dos retângulos
+function limits(min, max) {
     let start = Math.floor(Math.random() * (max - min)) + min;
     let end = Math.floor(Math.random() * (max - start)) + start + 1;
 
-    rect.style.height = '40px';
-    rect.style.backgroundColor = '#F7F2EA';
-
-    rect.style.gridColumn = `${start} / ${end}`;
-    rect.style.gridRow = "6";
-
-    selected.appendChild(rect);
-
-    return rect;
+    return { start, end };
 }
