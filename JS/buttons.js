77d35d80d6193e4d1fb990b12f1c80d116abae6a
MariaMiguel0510 <mariammpedro04@gmail.com>
2026-09-20 11:26:22 +0100

    LandingPage testeipad17

--- JS/buttons.js
+++ JS/buttons.js
@@ -17,10 +17,10 @@ let atlas_container = document.querySelector(".atlas_grid_container");
 
 
 //OPEN/CLOSE ABOUT/ARCHIVE ---------------------------------------------------
-toggleSection(open_about, open_archive, close_about, about_container, 'right', '100vw');//about
-toggleSection(open_archive, open_about, close_archive, archive_container, 'left', '-100vw');//archive
+toggleSection(open_about, close_about, about_container, 'right', '100vw');//about
+toggleSection(open_archive, close_archive, archive_container, 'left', '-100vw');//archive
 
-function toggleSection(openBtn, oppositeBtn, closeBtn, container, direction, landingMove) {
+function toggleSection(openBtn, closeBtn, container, direction, landingMove) {
     //coloca a transicao de abertura universal
     container.style.transition = `${direction} 0.9s ease`;
     landing_page.style.transition = `left 0.9s ease`;
@@ -45,8 +45,11 @@ function toggleSection(openBtn, oppositeBtn, closeBtn, container, direction, lan
         container.style[direction] = '100vw';
         landing_page.style.left = '0vw';
         lockScroll(false);//ativa o scroll vertical
-        oppositeBtn.style.color = 'red';
     });
+
+    // coloca  os botões ao estado normal
+    open_about.style.color = 'black';
+    open_archive.style.color = 'black';
 }
 
 
