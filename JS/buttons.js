4adf9bbd6ddd9bc5be8b08058126794c4e6f52c4
MariaMiguel0510 <mariammpedro04@gmail.com>
2026-09-20 11:45:46 +0100

    LandingPage testeipad20

--- JS/buttons.js
+++ JS/buttons.js
@@ -26,7 +26,7 @@ function toggleSection(openBtn, oppositeopenBtn, closeBtn, oppositecloseBtn, con
     landing_page.style.transition = `left 0.9s ease`;
 
 
-    closeBtn.addEventListener('mouseover', function () {
+    closeBtn.addEventListener('mouseout', function () {
         oppositeopenBtn.style.color = 'black';
     });
 
