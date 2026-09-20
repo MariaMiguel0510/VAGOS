0aeeaf87a91a1abb519cdd2f7c0044b0eaef2309
MariaMiguel0510 <mariammpedro04@gmail.com>
2026-09-20 11:36:35 +0100

    LandingPage testeipad18.1

--- JS/buttons.js
+++ JS/buttons.js
@@ -17,8 +17,8 @@ let atlas_container = document.querySelector(".atlas_grid_container");
 
 
 //OPEN/CLOSE ABOUT/ARCHIVE ---------------------------------------------------
-toggleSection(open_about, close_about, about_container, 'right', '100vw');//about
-toggleSection(open_archive, close_archive, archive_container, 'left', '-100vw');//archive
+toggleSection(open_about, open_archive, close_about, about_container, 'right', '100vw');//about
+toggleSection(open_archive, open_about, close_archive, archive_container, 'left', '-100vw');//archive
 /*
 function toggleSection(openBtn, closeBtn, container, direction, landingMove) {
     //coloca a transicao de abertura universal
